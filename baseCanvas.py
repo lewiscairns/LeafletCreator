6e07cd5b00187a025d903962ca7241e88cf59f26
Lewis Cairns <lewisc@vivaldi.net>
2023-01-17 17:50:20 +0000

    Added text boxes to canvas

--- baseCanvas.py
+++ baseCanvas.py
@@ -38,14 +38,24 @@ class BlankPage(tk.Frame):
     def __init__(self, master, number):
         super().__init__(master)
 
-        self.label = tk.Label(self, text="This is a blank page " + str(number))
+        self.label = tk.Label(self, text="Page " + str(number))
         self.label.pack()
+        
+        self.text_box_1 = tk.Text(self, height = 5, width = 52)
+        self.text_box_1.pack()
+        self.text_box_2 = tk.Text(self, height = 5, width = 52)
+        self.text_box_2.pack()
+        self.text_box_3 = tk.Text(self, height = 5, width = 52)
+        self.text_box_3.pack()
+        self.text_box_4 = tk.Text(self, height = 5, width = 52)
+        self.text_box_4.pack()
 
         self.next_button = tk.Button(self, text="Next", command=master.next_page)
         self.next_button.pack()
 
         self.prev_button = tk.Button(self, text="Previous", command=master.prev_page)
         self.prev_button.pack()
+
         self.add_button = tk.Button(self, text="Add New Page", command=master.create_page)
         self.add_button.pack()
 
