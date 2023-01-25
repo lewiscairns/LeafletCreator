43c089230f7d9b87d7841ed188899cd2957a1d1f
Lewis Cairns <lewisc@vivaldi.net>
2023-01-25 15:25:03 +0000

    changed placement of page buttons

--- baseCanvas.py
+++ baseCanvas.py
@@ -40,39 +40,49 @@ class LeafletPage(tk.Frame):
 
         self.label = tk.Label(self, text="Page " + str(number))
         self.label.grid()
+
+        self.prev_button = tk.Button(self, text="Previous", command=master.prev_page)
+        self.prev_button.grid(row = 0, column = 2, padx = 30, pady=10)
+
+        self.add_button = tk.Button(self, text="Add New Page", command=master.create_page)
+        self.add_button.grid(row = 0, column = 1, padx = 30, pady=10)
+
+        self.next_button = tk.Button(self, text="Next", command=master.next_page)
+        self.next_button.grid(row = 0, column = 0, padx = 30, pady=10)
         
         self.defaultImage = "WikiNoImage.png"
 
         self.image1 = Image.open(self.defaultImage)
         self.photo1 = ImageTk.PhotoImage(self.image1)
         self.displayImage1 = tk.Label(self, image = self.photo1)
-        self.displayImage1.grid(row = 0, column = 0, padx = 60)
+        self.displayImage1.grid(row = 1, column = 0, padx = 60, pady=10)
         self.displayImage1.bind('<Button-1>', self.imageClick)
         self.displayImageButton1 = tk.Button(self, image = self.photo1, command = self.imageClick)
-        self.displayImage1.grid(row = 0, column = 0, padx = 60)
+        self.displayImage1.grid(row = 1, column = 0, padx = 60, pady=10)
         self.displayImageButton1 = tk.Button(self, text = "Close", command = self.destroy)
-        self.displayImage1.grid(row = 0, column = 0, padx = 20)
+        self.displayImage1.grid(row = 1, column = 0, padx = 60, pady=10)
+
+        self.image2 = Image.open(self.defaultImage)
+        self.photo2 = ImageTk.PhotoImage(self.image2)
+        self.displayImage2 = tk.Label(self, image = self.photo2)
+        self.displayImage2.grid(row = 2, column = 0, padx = 60, pady=10)
+        self.displayImage2.bind('<Button-1>', self.imageClick)
+        self.displayImageButton2 = tk.Button(self, image = self.photo1, command = self.imageClick)
+        self.displayImage2.grid(row = 2, column = 0, padx = 60, pady=10)
+        self.displayImageButton2 = tk.Button(self, text = "Close", command = self.destroy)
+        self.displayImage2.grid(row = 2, column = 0, padx = 60, pady=10)
 
         self.text_box_1 = tk.Text(self, height = 7, width = 52)
-        self.text_box_1.grid(row = 0, column = 1, padx = 60)
+        self.text_box_1.grid(row = 1, column = 1, padx = 60, pady=10)
 
         self.text_box_2 = tk.Text(self, height = 7, width = 52)
-        self.text_box_2.grid(row = 1, column = 1)
+        self.text_box_2.grid(row = 2, column = 1, padx = 60, pady=10)
 
         self.text_box_3 = tk.Text(self, height = 7, width = 52)
-        self.text_box_3.grid(row = 2, column = 1)
+        self.text_box_3.grid(row = 3, column = 1, padx = 60, pady=10)
 
         self.text_box_4 = tk.Text(self, height = 7, width = 52)
-        self.text_box_4.grid(row = 3, column = 1)
-
-        self.next_button = tk.Button(self, text="Next", command=master.next_page)
-        self.next_button.grid()
-
-        self.prev_button = tk.Button(self, text="Previous", command=master.prev_page)
-        self.prev_button.grid()
-
-        self.add_button = tk.Button(self, text="Add New Page", command=master.create_page)
-        self.add_button.grid()
+        self.text_box_4.grid(row = 4, column = 1, padx = 60, pady=10)
 
     def imageClick(self, event = None):
         print('clicked')
