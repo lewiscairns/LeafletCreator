eecd957c5ee658108574d028eb9dd7b1646b6336
Lewis Cairns <lewisc@vivaldi.net>
2023-01-27 17:05:36 +0000

    Changed naming convention

--- baseCanvas.py
+++ baseCanvas.py
@@ -6,6 +6,7 @@ import docxFiles
 
 pageCounter = 1
 
+
 class LeafletCreator(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -38,74 +39,76 @@ class LeafletCreator(tk.Tk):
         self.show_page()
 
     def generate(self):
-        allRows = [[]]
-        pageCounter = 0
+        all_rows = [[]]
+        page_counter = 0
         for page in self.pages:
-            allRows.append([pageCounter])
+            all_rows.append([page_counter])
             for row in page.row1, page.row2, page.row3, page.row4:
-                label, text = row.getRow()
-                allRows[pageCounter].append([label, self.retrieveInput(text)])
-            pageCounter = pageCounter + 1
-        allRows.pop(1)
-        docxFiles.createDocument("Lets go running", allRows)
+                label, text = row.get_row()
+                all_rows[page_counter].append([label, self.retrieve_input(text)])
+            page_counter = page_counter + 1
+        all_rows.pop(1)
+        docxFiles.create_document("Lets go running", all_rows)
         tk.messagebox.showinfo("Success", "Your document has been created, please open it in Word")
- 
-    def retrieveInput(self, textBox):
-        inputValue = textBox.get("1.0","end-1c")
-        return inputValue
+
+    @staticmethod
+    def retrieve_input(text_box):
+        input_value = text_box.get("1.0", "end-1c")
+        return input_value
+
 
 class LeafletPage(tk.Frame):
     def __init__(self, master, number):
         super().__init__(master)
 
         self.label = tk.Label(self, text="Page " + str(number))
-        self.label.grid(row = 0, column=1, pady=20)
+        self.label.grid(row=0, column=1, pady=20)
 
         self.prev_button = tk.Button(self, text="Previous", command=master.prev_page)
-        self.prev_button.grid(row = 1, column = 0, padx = 30, pady=10)
+        self.prev_button.grid(row=1, column=0, padx=30, pady=10)
 
         self.add_button = tk.Button(self, text="Add New Page", command=master.create_page)
-        self.add_button.grid(row = 1, column = 1, padx = 30, pady=10)
+        self.add_button.grid(row=1, column=1, padx=30, pady=10)
 
         self.next_button = tk.Button(self, text="Next", command=master.next_page)
-        self.next_button.grid(row = 1, column = 2, padx = 30, pady=10)
+        self.next_button.grid(row=1, column=2, padx=30, pady=10)
 
         self.generate_button = tk.Button(self, text="Generate", command=master.generate)
-        self.generate_button.grid(row = 1, column = 3, padx = 30, pady=10)
-        
-        self.row1 = pageRow(self, 2)
-        self.row2 = pageRow(self, 3)
-        self.row3 = pageRow(self, 4)
-        self.row4 = pageRow(self, 5)
+        self.generate_button.grid(row=1, column=3, padx=30, pady=10)
 
-class pageRow:
-    def __init__(self, master, rowNum):
+        self.row1 = PageRow(self, 2)
+        self.row2 = PageRow(self, 3)
+        self.row3 = PageRow(self, 4)
+        self.row4 = PageRow(self, 5)
+
+
+class PageRow:
+    def __init__(self, master, row_num):
         self.filename = "WikiNoImage.png"
         self.image = Image.open(self.filename)
         self.resizeImage = self.image.resize((150, 150))
         self.photo = ImageTk.PhotoImage(self.resizeImage)
-        self.labelImage = tk.Label(master, image = self.photo)
+        self.labelImage = tk.Label(master, image=self.photo)
         self.labelImage.image = self.photo
-        self.labelImage.bind("<Button-1>", self.imageClick)
+        self.labelImage.bind("<Button-1>", self.image_click)
         self.theMaster = master
-        
-        self.text_box = tk.Text(master, height = 9, width = 52)
 
-        self.labelImage.grid(row = rowNum, column = 0, padx = 60, pady=10)
-        self.text_box.grid(row = rowNum, column = 1, padx = 60, pady=10)
+        self.text_box = tk.Text(master, height=9, width=52)
 
-    def imageClick(self, event = None):
+        self.labelImage.grid(row=row_num, column=0, padx=60, pady=10)
+        self.text_box.grid(row=row_num, column=1, padx=60, pady=10)
+
+    def image_click(self, event=None):
         filetypes = (('image png', '*.png'), ('image jpg', '*.jpg'), ('All files', '*.*'))
-        self.filename = fd.askopenfilename(title = 'Open Images', initialdir = '/', filetypes = filetypes)
+        self.filename = fd.askopenfilename(title='Open Images', initialdir='/', filetypes=filetypes)
         print(self.filename)
         self.image = Image.open(self.filename)
         self.resizeImage = self.image.resize((150, 150))
         self.photo = ImageTk.PhotoImage(self.resizeImage)
-        self.labelImage.configure(image = self.photo)
+        self.labelImage.configure(image=self.photo)
         self.labelImage.image = self.photo
-        self.defaultImage = self.filename
 
-    def getRow(self):
+    def get_row(self):
         return self.filename, self.text_box
 
 
--- docxFiles.py
+++ docxFiles.py
@@ -1,16 +1,17 @@
 from docx import Document
 from docx.shared import Inches
 
-def createDocument(title, allRows):
+
+def create_document(title, all_rows):
     document = Document()
     document.add_heading(title, 0)
-    for page in allRows:
+    for page in all_rows:
         table = document.add_table(rows=0, cols=2)
         for row in page[0:]:
             row_cells = table.add_row().cells
             image = row_cells[0].paragraphs[0]
             text = row_cells[1].paragraphs[0]
             run = image.add_run()
-            run.add_picture(row[0], width = Inches(1.75))
+            run.add_picture(row[0], width=Inches(1.75))
             text.add_run(row[1])
     document.save(title + '.docx')
