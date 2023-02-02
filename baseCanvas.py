c78e5d708c472532d3d1ef895a26171fc743eaf7
Lewis Cairns <lewisc@vivaldi.net>
2023-02-02 15:02:28 +0000

    User can select where to save and load

--- baseCanvas.py
+++ baseCanvas.py
@@ -4,7 +4,7 @@ from tkinter.messagebox import showinfo
 from tkinter.simpledialog import askstring
 from PIL import Image, ImageTk
 import docxFiles
-import pickle as pk
+import pickle as lc
 
 pageCounter = 1
 
@@ -22,6 +22,8 @@ class LeafletCreator(tk.Tk):
         self.create_page()
 
         self.user_title = "Untitled"
+
+        self.saved_folder = ""
 
     def title_file(self):
         self.user_title = tk.simpledialog.askstring("File Name", "Please enter a title for this file")
@@ -64,9 +66,10 @@ class LeafletCreator(tk.Tk):
     def move_page(self):
         print("Move Page")
 
-    def save(self):
+    def save_as(self):
         try:
-            with open("save.pk", "wb") as file:
+            self.saved_folder = fd.askdirectory(title="Select Folder To Generate File")
+            with open(self.saved_folder + "/" + self.user_title + ".lc", "wb") as file:
                 pages_text = []
                 for page in self.pages:
                     page_text = []
@@ -75,16 +78,35 @@ class LeafletCreator(tk.Tk):
                         page_text.append([label, self.retrieve_input(text)])
                     pages_text.append(page_text)
                 data = [self.user_title, pages_text]
-                pk.dump(data, file, protocol=pk.HIGHEST_PROTOCOL)
+                lc.dump(data, file, protocol=lc.HIGHEST_PROTOCOL)
+                tk.messagebox.showinfo("Success", "Your document has been saved")
+        except Exception as e:
+            print("Error: " + str(e))
+
+    def save(self):
+        try:
+            with open(self.saved_folder + "/" + self.user_title + ".lc", "wb") as file:
+                pages_text = []
+                for page in self.pages:
+                    page_text = []
+                    for row in page.row1, page.row2, page.row3, page.row4:
+                        label, text = row.get_row()
+                        page_text.append([label, self.retrieve_input(text)])
+                    pages_text.append(page_text)
+                data = [self.user_title, pages_text]
+                lc.dump(data, file, protocol=lc.HIGHEST_PROTOCOL)
         except Exception as e:
             print("Error: " + str(e))
 
     def load(self):
         self.pages[self.current_page].grid_forget()
+        filetypes = (('leaflet.lc', '*.lc'), ('All files', '*.*'))
+        filename = fd.askopenfilename(title='Open Leaflet', initialdir='/', filetypes=filetypes)
         try:
-            with open("save.pk", "rb") as file:
-                data = pk.load(file)
+            with open(filename, "rb") as file:
+                data = lc.load(file)
                 self.user_title = data[0]
+                self.title = (self.user_title + " - Leaflet Creator")
                 self.pages = []
                 for page in data[1]:
                     self.pages.append(LeafletPage(self))
@@ -129,6 +151,9 @@ class LeafletPage(tk.Frame):
         self.load_button = tk.Button(self, text="Load", command=master.load)
         self.load_button.grid(row=2, column=1, padx=30, pady=10)
 
+        self.save_as_button = tk.Button(self, text="Save As", command=master.save_as)
+        self.save_as_button.grid(row=2, column=2, padx=30, pady=10)
+
         self.row1 = PageRow(self, 3)
         self.row2 = PageRow(self, 4)
         self.row3 = PageRow(self, 5)
@@ -150,6 +175,7 @@ class LeafletPage(tk.Frame):
         else:
             print("Error: No more rows available")
 
+
 class PageRow:
     def __init__(self, master, row_num):
         self.filename = "WikiNoImage.png"
@@ -169,7 +195,6 @@ class PageRow:
     def image_click(self, event=None):
         filetypes = (('image png', '*.png'), ('image jpg', '*.jpg'), ('All files', '*.*'))
         self.filename = fd.askopenfilename(title='Open Images', initialdir='/', filetypes=filetypes)
-        print(self.filename)
         self.image = Image.open(self.filename)
         self.resizeImage = self.image.resize((150, 150))
         self.photo = ImageTk.PhotoImage(self.resizeImage)
