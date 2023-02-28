9140602d0019c635e359745f6621d67e3c57863b
Lewis Cairns <lewisc@vivaldi.net>
2023-02-28 17:40:59 +0000

    Added sentenceAnalysis.py

--- baseCanvas.py
+++ baseCanvas.py
@@ -3,17 +3,14 @@ import sys
 
 import fileIO
 import menuBar
+import sentenceAnalysis
 
 import tkinter as tk
 from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
-from tkinter.simpledialog import askstring
 
 import numpy as np
-import textstat
 import re
 from textblob import Word
-from textblob import TextBlob
 from PIL import Image, ImageTk
 from nltk.corpus import wordnet
 
@@ -232,73 +229,8 @@ class PageRow:
         self.text = ""
         self.num_spaces = 0
 
-    def show_complexity_recommendations(self, event):
-        recommendation_text = ""
-        for recommendation in self.complexity_recommendations:
-            if recommendation != "":
-                recommendation_text += "\n" + recommendation
-        if recommendation_text == "":
-            recommendation_text = "No recommendations available"
-        top = tk.Toplevel(self.master)
-        top.geometry("300x150")
-        top.title("Recommendations")
-        top_label = tk.Label(top, text=recommendation_text)
-        top_button = tk.Button(top, text="Close", command=top.destroy)
-        top_label.pack()
-        top_button.pack()
-
-    def check_sentence(self, event):
-        self.reading_level = textstat.flesch_reading_ease(self.text_box.get("1.0", "end-1c"))
-        if self.reading_level < self.leaflet_master.reading_level:
-            self.sentence_issues[0] = True
-            self.complexity_recommendations[0] = "Reading level is: " + str(
-                round(self.reading_level)) + "\nTry keep it below " + str(self.leaflet_master.reading_level) + ".\n"
-        elif self.reading_level > self.leaflet_master.reading_level:
-            self.sentence_issues[0] = False
-            self.complexity_recommendations[0] = ""
-
-        self.word_count = len(self.text_box.get("1.0", "end-1c").split())
-        if self.word_count > self.leaflet_master.word_count:
-            self.sentence_issues[1] = True
-            self.complexity_recommendations[1] = "Current word count is: " + str(
-                self.word_count) + "\nTry keep it below " + str(self.leaflet_master.word_count) + " words.\n"
-        elif self.word_count < self.leaflet_master.word_count:
-            self.sentence_issues[1] = False
-            self.complexity_recommendations[1] = ""
-
-        self.polarity = TextBlob(self.text_box.get("1.0", "end-1c")).sentiment.polarity
-        if self.polarity < self.leaflet_master.polarity:
-            self.sentence_issues[2] = True
-            self.complexity_recommendations[2] = "Current polarity (how positive your sentence is) is: " + str(
-                round(self.polarity, 2)) + "\nTry keep it above " + str(self.leaflet_master.polarity) + ".\n"
-        elif self.polarity > self.leaflet_master.polarity:
-            self.sentence_issues[2] = False
-            self.complexity_recommendations[2] = ""
-
-        self.update_complexity()
-
-    def update_complexity(self):
-        if np.count_nonzero(self.sentence_issues) > 1:
-            self.sentence_complexity = "Bad"
-        elif np.count_nonzero(self.sentence_issues) > 0:
-            self.sentence_complexity = "Average"
-        else:
-            self.sentence_complexity = "Good"
-        self.update_complexity_image()
-
-    def update_complexity_image(self):
-        if self.sentence_complexity == "Good":
-            self.complexity_filename = "WikiGreenCircle.png"
-        elif self.sentence_complexity == "Average":
-            self.complexity_filename = "WikiYellowCircle.png"
-        else:
-            self.complexity_filename = "WikiRedCircle.png"
-
-        self.complexity_image = Image.open(self.complexity_filename)
-        self.complexity_resize_image = self.complexity_image.resize((100, 100))
-        self.complexity_photo = ImageTk.PhotoImage(self.complexity_resize_image)
-        self.complexity_icon.configure(image=self.complexity_photo)
-        self.complexity_icon.image = self.complexity_photo
+    def get_row(self):
+        return self.filename, self.text_box
 
     def image_click(self, event=None):
         filetypes = (('image png', '*.png'), ('image jpg', '*.jpg'), ('All files', '*.*'))
@@ -309,8 +241,11 @@ class PageRow:
         self.label_image.configure(image=self.photo)
         self.label_image.image = self.photo
 
-    def get_row(self):
-        return self.filename, self.text_box
+    def check_sentence(self, event):
+        sentenceAnalysis.check_sentence(self)
+
+    def show_complexity_recommendations(self, event):
+        sentenceAnalysis.show_complexity_recommendations(self)
 
     def word_complexity_check(self):
         for word in self.text.split(' '):
@@ -405,186 +340,6 @@ class PageRow:
                         self.text_box.tag_remove("wrong", f'1.{position}', f'1.{position + len(word)}')
                         self.misspelled_tag.remove(position)
             self.word_complexity_check()
-
-
-class ChangeFont:
-    def __init__(self, master):
-        self.master = master
-        self.top = tk.Toplevel(master)
-        self.top.geometry("300x150")
-        self.top.title("Font Style")
-        self.arial_button = tk.Button(self.top, text="Arial", command=self.arial)
-        self.times_button = tk.Button(self.top, text="Times New Roman", command=self.times)
-        self.arial_button.pack()
-        self.times_button.pack()
-
-    def arial(self):
-        self.master.font_style = "Arial"
-        self.top.destroy()
-        tk.messagebox.showinfo("Success", "Your document will now generate in Arial as the font style")
-
-    def times(self):
-        self.master.font_style = "Times New Roman"
-        self.top.destroy()
-        tk.messagebox.showinfo("Success", "Your document will now generate in Times New Roman as the font style")
-
-
-class ChangeFontSize:
-    def __init__(self, master):
-        self.master = master
-        self.top = tk.Toplevel(master)
-        self.top.geometry("300x150")
-        self.top.title("Font Size")
-        self.top_label = tk.Label(self.top, text="Recommended Font Size is 16")
-        self.top_button_up = tk.Button(self.top, text="+", command=self.increase_font_size)
-        self.top_button_down = tk.Button(self.top, text="-", command=self.decrease_font_size)
-        self.top_font_size = tk.IntVar()
-        self.top_font_size.set(self.master.font_size)
-        self.top_entry = tk.Entry(self.top, textvariable=self.top_font_size)
-        self.top_entry.configure(state="readonly")
-        self.top_button = tk.Button(self.top, text="Close", command=self.top.destroy)
-        self.top_label.grid(row=0, column=0, columnspan=2)
-        self.top_button_up.grid(row=1, column=0)
-        self.top_entry.grid(row=1, column=1)
-        self.top_button_down.grid(row=1, column=2)
-        self.top_button.grid(row=2, column=0, columnspan=2)
-
-    def increase_font_size(self):
-        self.master.font_size = self.master.font_size + 1
-        self.top_font_size.set(self.master.font_size)
-
-    def decrease_font_size(self):
-        self.master.font_size = self.master.font_size - 1
-        self.top_font_size.set(self.master.font_size)
-
-
-class ChangeReading:
-    def __init__(self, master):
-        self.master = master
-        self.top = tk.Toplevel(master)
-        self.top.geometry("300x150")
-        self.top.title("Reading Level")
-        self.top_label = tk.Label(self.top, text="Recommended Reading Level is 90")
-        self.top_button_up = tk.Button(self.top, text="+", command=self.increase_reading_level)
-        self.top_button_down = tk.Button(self.top, text="-", command=self.decrease_reading_level)
-        self.top_reading_level = tk.IntVar()
-        self.top_reading_level.set(self.master.reading_level)
-        self.top_entry = tk.Entry(self.top, textvariable=self.top_reading_level)
-        self.top_entry.configure(state="readonly")
-        self.top_button = tk.Button(self.top, text="Close", command=self.top.destroy)
-        self.top_label.grid(row=0, column=0, columnspan=2)
-        self.top_button_up.grid(row=1, column=0)
-        self.top_entry.grid(row=1, column=1)
-        self.top_button_down.grid(row=1, column=2)
-        self.top_button.grid(row=2, column=0, columnspan=2)
-
-    def increase_reading_level(self):
-        self.master.reading_level = self.master.reading_level + 1
-        self.top_reading_level.set(self.master.reading_level)
-
-    def decrease_reading_level(self):
-        self.master.reading_level = self.master.reading_level - 1
-        self.top_reading_level.set(self.master.reading_level)
-
-
-class ChangeWordCount:
-    def __init__(self, master):
-        self.master = master
-        self.top = tk.Toplevel(master)
-        self.top.geometry("300x150")
-        self.top.title("Word Count")
-        self.top_label = tk.Label(self.top, text="Recommended Word Count is 10")
-        self.top_button_up = tk.Button(self.top, text="+", command=self.increase_word_count)
-        self.top_button_down = tk.Button(self.top, text="-", command=self.decrease_word_count)
-        self.top_word_count = tk.IntVar()
-        self.top_word_count.set(self.master.word_count)
-        self.top_entry = tk.Entry(self.top, textvariable=self.top_word_count)
-        self.top_entry.configure(state="readonly")
-        self.top_button = tk.Button(self.top, text="Close", command=self.top.destroy)
-        self.top_label.grid(row=0, column=0, columnspan=2)
-        self.top_button_up.grid(row=1, column=0)
-        self.top_entry.grid(row=1, column=1)
-        self.top_button_down.grid(row=1, column=2)
-        self.top_button.grid(row=2, column=0, columnspan=2)
-
-    def increase_word_count(self):
-        self.master.word_count = self.master.word_count + 1
-        self.top_word_count.set(self.master.word_count)
-
-    def decrease_word_count(self):
-        self.master.word_count = self.master.word_count - 1
-        self.top_word_count.set(self.master.word_count)
-
-
-class ChangePolarity:
-    def __init__(self, master):
-        self.master = master
-        self.top = tk.Toplevel(master)
-        self.top.geometry("300x150")
-        self.top.title("Sentiment Polarity")
-        self.top_label = tk.Label(self.top, text="Recommended Sentiment Polarity is 0")
-        self.top_label2 = tk.Label(self.top, text="Polarity is a number between -1 and 1")
-        self.top_button_up = tk.Button(self.top, text="+", command=self.increase_polarity_limit)
-        self.top_button_down = tk.Button(self.top, text="-", command=self.decrease_polarity_limit)
-        self.top_polarity = tk.IntVar()
-        self.top_polarity.set(self.master.polarity)
-        self.top_entry = tk.Entry(self.top, textvariable=self.top_polarity)
-        self.top_entry.configure(state="readonly")
-        self.top_button = tk.Button(self.top, text="Close", command=self.top.destroy)
-        self.top_label.grid(row=0, column=0, columnspan=2)
-        self.top_label2.grid(row=1, column=0, columnspan=2)
-        self.top_button_up.grid(row=2, column=0)
-        self.top_entry.grid(row=2, column=1)
-        self.top_button_down.grid(row=2, column=2)
-        self.top_button.grid(row=3, column=0, columnspan=2)
-
-    def increase_polarity_limit(self):
-        self.master.polarity = self.master.polarity + 0.1
-        self.top_polarity.set(self.master.polarity)
-
-    def decrease_polarity_limit(self):
-        self.master.polarity = self.master.polarity - 0.1
-        self.top_polarity.set(self.master.polarity)
-
-
-class Watermark:
-    def __init__(self, master):
-        self.master = master
-        self.watermark_image = self.master.watermark_image
-        self.top = tk.Toplevel(master)
-        self.top.geometry("300x150")
-        self.top.title("Watermark")
-        self.top_label = tk.Label(self.top, text="Please enter watermark text and image")
-        self.top_button = tk.Button(self.top, text="Close", command=self.top.destroy)
-        self.top_button_save = tk.Button(self.top, text="Save", command=self.save_watermark)
-        self.watermark_text = tk.StringVar()
-        self.watermark_text.set(self.master.watermark_text)
-        self.top_entry = tk.Entry(self.top, textvariable=self.watermark_text)
-        self.top_image = Image.open(self.watermark_image)
-        self.top_resize_image = self.top_image.resize((50, 50))
-        self.top_photo = ImageTk.PhotoImage(self.top_resize_image)
-        self.top_image_label = tk.Label(self.top, image=self.top_photo, cursor="hand2")
-        self.top_image_label.bind("<Button-1>", self.image_click)
-        self.top_label.grid(row=0, column=0, columnspan=2)
-        self.top_entry.grid(row=1, column=0, padx=5, pady=10)
-        self.top_image_label.grid(row=1, column=1, padx=10, pady=10)
-        self.top_button_save.grid(row=2, column=0, pady=10)
-        self.top_button.grid(row=2, column=1, pady=10)
-
-    def save_watermark(self):
-        self.master.watermark_text = self.top_entry.get()
-        self.master.watermark_image = self.watermark_image
-        self.top.destroy()
-
-    def image_click(self, event=None):
-        filetypes = (('image png', '*.png'), ('image jpg', '*.jpg'), ('All files', '*.*'))
-        self.watermark_image = fd.askopenfilename(title='Open Images', initialdir='C:/Program Files/LeafletCreator/Images', filetypes=filetypes)
-        self.top_image = Image.open(self.watermark_image)
-        self.top_resize_image = self.top_image.resize((50, 50))
-        self.top_photo = ImageTk.PhotoImage(self.top_resize_image)
-        self.top_image_label.configure(image=self.top_photo)
-        self.top_image_label.image = self.top_photo
-        self.top.lift()
 
 
 def new_file():
