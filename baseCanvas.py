b3014ef9eef17bff6be4e549209119ed21a38702
Lewis Cairns <lewisc@vivaldi.net>
2023-03-09 17:55:00 +0000

    Fullscreen added

--- baseCanvas.py
+++ baseCanvas.py
@@ -20,6 +20,7 @@ class LeafletCreator(tk.Tk):
 
         self.title("Untitled - Leaflet Creator")
         self.geometry("1000x900")
+        self.state('zoomed')
 
         self.pages = []
         self.current_page = 0
