f8aac4ff12c0713eca47fe6275ac6414a3fb76de
Lewis Cairns <lewisc@vivaldi.net>
2023-04-22 19:51:59 +0100

    commented rightClickMenu

--- rightClickMenu.py
+++ rightClickMenu.py
@@ -6,40 +6,59 @@ from nltk.corpus import wordnet
 import re
 
 
+# This function checks the spelling of the text
 def check_spelling(self):
+    # Get the text from the text box
     self.text = self.text_box.get("1.0", tk.END)
-    current_spaces = self.text.count(' ')
     self.misspelled_tag = []
-    if current_spaces != self.num_spaces:
-        self.num_spaces = current_spaces
-        for word in self.text.split(' '):
-            word = word.lower()
-            word_positions = [word_start for word_start in range(len(self.text)-len(word)+1) if self.text[word_start:word_start+len(word)] == word]
-            suggestion = Word.spellcheck(Word(word))
-            suggestion_text = suggestion[0]
-            suggestion_text = str(suggestion_text).split(" ", 1)[0]
-            suggestion_text = self.regex.sub('', suggestion_text)
-            word = re.sub(r'[.;:,?"!/]', '', word)
-            word_parsed = self.regex.sub('', word)
-            for position in word_positions:
-                self.misspelled_tag.append(position)
-                word_found = find_word_in_list(suggestion, word_parsed)
-                if suggestion_text != "n" and word_found is False and word_parsed != "\n" and word_parsed.isspace() is False and len(word_parsed) \
-                        != 1 and word not in self.leaflet_master.ignore_words and word != "-":
-                    self.text_box.tag_add("wrong", f'1.{position}', f'1.{position + len(word)}')
-                else:
-                    self.text_box.tag_remove("wrong", f'1.{position}', f'1.{position + len(word)}')
-                    self.misspelled_tag.remove(position)
-        word_complexity_check(self)
 
-
-def word_complexity_check(self):
+    # Loop through each word in the text
     for word in self.text.split(' '):
+        # Make the word lowercase and get its position
+        word = word.lower()
+        word_positions = [word_start for word_start in range(len(self.text)-len(word)+1) if self.text[word_start:word_start+len(word)] == word]
+
+        # Get the suggestion to replace the word and parse it
+        suggestion = Word.spellcheck(Word(word))
+        suggestion_text = suggestion[0]
+        suggestion_text = str(suggestion_text).split(" ", 1)[0]
+        suggestion_text = self.regex.sub('', suggestion_text)
+
+        # Parse the word
+        word = re.sub(r'[.;:,?"!/]', '', word)
+        word_parsed = self.regex.sub('', word)
+
+        # Loop through each position of the word
+        for position in word_positions:
+
+            # Store the words position
+            self.misspelled_tag.append(position)
+            word_found = find_word_in_list(suggestion, word_parsed)
+
+            # Check if the word is misspelled and add the wrong tag to it
+            if suggestion_text != "n" and word_found is False and word_parsed != "\n" and word_parsed.isspace() is False and len(word_parsed) \
+                    != 1 and word not in self.leaflet_master.ignore_words and word != "-":
+                self.text_box.tag_add("wrong", f'1.{position}', f'1.{position + len(word)}')
+            else:
+                # Remove the wrong tag and word position if the word is not misspelled
+                self.text_box.tag_remove("wrong", f'1.{position}', f'1.{position + len(word)}')
+                self.misspelled_tag.remove(position)
+    word_complexity_check(self)
+
+
+# This function checks the complexity of the text
+def word_complexity_check(self):
+    # Get the text from the text box
+    for word in self.text.split(' '):
+        # Get the position of the word, and parse it
         word_positions = [word_start for word_start in range(len(self.text)-len(word)+1) if self.text[word_start:word_start+len(word)] == word]
         word = word.lower()
         word = re.sub(r'[.;:,?"!/]', '', word)
         word_parsed = self.regex.sub('', word)
+
+        # Loop through each position of the word
         for position in word_positions:
+            # Give the word a wrong tag if it is not there is no synonym for it
             if get_word_synonym(self, word_parsed) and len(self.synonyms) == 0 and word not in self.leaflet_master.ignore_words and word != "-":
                 self.text_box.tag_remove("uncommon", f'1.{position}', f'1.{position + len(word)}')
                 self.text_box.tag_add("wrong", f'1.{position}', f'1.{position + len(word)}')
@@ -50,6 +69,7 @@ def word_complexity_check(self):
                 self.text_box.tag_add("uncommon", f'1.{position}', f'1.{position + len(word)}')
 
 
+# This function checks if the word is the same as the suggestion
 def find_word_in_list(suggestion, word):
     for suggestionWord in suggestion:
         if word == suggestionWord[0]:
@@ -57,20 +77,28 @@ def find_word_in_list(suggestion, word):
     return False
 
 
+# This function generates the right-click menu
 def word_right_click(self, event):
+
+    # Get the word that was right-clicked
     word = self.text_box.get("@%d,%d wordstart" % (event.x, event.y), "@%d,%d wordend" % (event.x, event.y))
     self.text_box.mark_set("insert", "@%d,%d" % (event.x, event.y))
+
+    # Set the selection to the word that was right-clicked
     self.text_box.mark_set("sel.first", "insert wordstart")
     self.text_box.mark_set("sel.last", "insert wordend")
     wrong_ranges = self.text_box.tag_ranges("wrong")
     uncommon_ranges = self.text_box.tag_ranges("uncommon")
     word_menu = tk.Menu(self.master, tearoff=0)
+
+    # Add the commands to the right-click menu
     word_menu.add_command(label="Copy", command=lambda: copy_word(self))
     word_menu.add_command(label="Paste", command=lambda: paste_word(self))
     word_menu.add_command(label="Bold", command=lambda: bold_word(self))
     is_wrong = False
     is_uncommon = False
 
+    # Check if the word is misspelled or uncommon
     for i in range(0, len(wrong_ranges), 2):
         if str(self.text_box.index("sel.first")) == str(wrong_ranges[i]):
             is_wrong = True
@@ -83,6 +111,7 @@ def word_right_click(self, event):
             break
 
     try:
+        # Add the replace word and ignore warning commands to the right-click menu
         if is_wrong:
             if self.replacement_word != "":
                 word_menu.add_separator()
@@ -97,14 +126,18 @@ def word_right_click(self, event):
             word_menu.add_command(label="ignore warning", command=lambda: ignore_word(self, word))
     except IndexError:
         print("Right click error")
+    
+    # Popup the right-click menu
     word_menu.tk_popup(event.x_root, event.y_root, 0)
 
 
+# This function allows warning for the word to be ignored
 def ignore_word(self, word):
     self.leaflet_master.ignore_words.append(word)
     check_spelling(self)
 
 
+# This function gets the word replacement for the word
 def get_word_replacement(self, word):
     text = Word(word)
     suggestion = text.spellcheck()
@@ -120,6 +153,7 @@ def get_word_replacement(self, word):
         return False
 
 
+# This function gets the synonym for the word
 def get_word_synonym(self, word):
     self.synonyms = []
     if word in self.leaflet_master.common_words or word in self.leaflet_master.ignore_words \
@@ -132,27 +166,32 @@ def get_word_synonym(self, word):
         return True
 
 
+# This function replaces the word with the suggested word
 def replace_word(self):
     self.text_box.delete("sel.first", "sel.last")
     self.text_box.insert("sel.first", self.replacement_word)
     check_spelling(self)
 
 
+# This function replaces the word with the synonym
 def replace_synonym(self):
     self.text_box.delete("sel.first", "sel.last")
     self.text_box.insert("sel.first", self.synonyms[1])
     self.leaflet_master.ignore_words.append(self.synonyms[1])
 
 
+# This function copies the word to the clipboard
 def copy_word(self):
     self.text_box.clipboard_clear()
     self.text_box.clipboard_append(self.text_box.selection_get())
 
 
+# This function pastes the word from the clipboard
 def paste_word(self):
     self.text_box.insert(tk.INSERT, self.text_box.clipboard_get())
 
 
+# This function marks the word as bold, and makes it bold
 def bold_word(self):
     ranges = self.text_box.tag_ranges("bold")
     mark = self.text_box.index("sel.first")
